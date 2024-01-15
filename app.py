2a623554fd6cca6e974a07a3f548d1cc8199f381
Carlos José Ortiz Bonill <carlosortiz@192.168.0.5>
2024-01-15 17:34:28 -0500

    final demo

--- app.py
+++ app.py
@@ -8,16 +8,38 @@ st.set_page_config(layout='wide')
 
 st.title("Presupuesto General de la Nación")
 
-
-
 df = pd.read_csv('data/gastos.csv')
-rels_or = pd.read_csv('data/relationships_or.csv')
-rels = pd.read_csv('data/relationships.csv')
+df['PGN'] = 'PGN'
+
+year = st.select_slider("Seleccione un año", df['year'].unique())
+
+df = df[df['year'] == year]
+
+
 
 with open('data/dic_info.json', 'r') as js:
     dic_info = json.load(js)
 
-year = st.select_slider("Seleccione un año", df['year'].unique())
+dic_info['PGN'] = max(dic_info.values()) + 1
+
+rel_sec_ent = (df
+ .groupby(['year', 'sector name', 'entity name'])['cop']
+ .sum()
+ .reset_index()
+ .rename(columns={'sector name': 'col0',
+                  'entity name': 'col1'}))
+
+rel_ent_it = (df
+ .groupby(['year', 'entity name', 'item'])['cop']
+ .sum()
+ .reset_index()
+ .rename(columns={'entity name': 'col0',
+                  'item': 'col1'}))
+
+rels = pd.concat([rel_sec_ent, rel_ent_it])
+
+rels['col0'] = rels['col0'].map(dic_info)
+rels['col1'] = rels['col1'].map(dic_info)
 
 df = df[df['year'] == year]
 rels = rels[rels['year'] == year]
@@ -34,17 +56,74 @@ fig.update_layout(margin = dict(t=50, l=25, r=25, b=25))
 
 st.plotly_chart(fig)
 
+st.header('Presupuesto general')
+
+dic_info['PGN'] = 327
+test1 = (df
+         .groupby(['year', 'sector name', 'item'])['cop']
+         .sum()
+         .reset_index()
+         .rename(columns={'sector name':'col0',
+                          'item':'col1'}))
+test2 = (df
+         .groupby(['year','PGN', 'sector name'])['cop']
+         .sum()
+         .reset_index()
+         .rename(columns={'sector name':'col1',
+                          'PGN':'col0'}))
+test = pd.concat([test1, test2], ignore_index=True)
+test = test[test['year'] == year]
+
+test['col1'] = test['col1'].map(dic_info)
+test['col0'] = test['col0'].map(dic_info)
+
+fig = go.Figure(data=[go.Sankey(
+    node = dict(
+      pad = 15,
+      thickness = 20,
+      line = dict(color = "black", width = 0.5),
+      label = list(dic_info.keys()),
+      color = "blue"
+    ),
+    link = dict(
+      source = test['col0'], # indices correspond to labels, eg A1, A2, A1, B1, ...
+      target = test['col1'],
+      value = test['cop']
+  ))])
+
+fig.update_layout(title_text="Presupuesto general", font_size=10, width=1000, height=700)
+st.plotly_chart(fig)
+
+
+
 sector = st.selectbox("Seleccione el sector", 
                        df['sector name'].unique())
 
 df = df[df['sector name'] == sector]
-rels = rels[(rels['col0'] == dic_info[sector])]
+rel_sec_ent = (df
+ .groupby(['year', 'sector name', 'entity name'])['cop']
+ .sum()
+ .reset_index()
+ .rename(columns={'sector name': 'col0',
+                  'entity name': 'col1'}))
+
+rel_ent_it = (df
+ .groupby(['year', 'entity name', 'item'])['cop']
+ .sum()
+ .reset_index()
+ .rename(columns={'entity name': 'col0',
+                  'item': 'col1'}))
+
+rels = pd.concat([rel_sec_ent, rel_ent_it])
+
+rels['col0'] = rels['col0'].map(dic_info)
+rels['col1'] = rels['col1'].map(dic_info)
 
 
-col1, col2 = st.columns(2)
 
-with col1:
-    fig = go.Figure(data=[go.Sankey(
+
+
+fig = go.Figure(data=[go.Sankey(
     node = dict(
         pad = 15,
         thickness = 30,
@@ -57,36 +136,12 @@ with col1:
         value = rels['cop']
         ))])
 
-    fig.update_layout(title_text="PGN", font_size=10, width=500, height=350)
-    st.header('Sankey diagram')
-    st.plotly_chart(fig)
+fig.update_layout(title_text="Presupueso por sector", font_size=10, width=1000, height=800)
+st.header('Presupuesto por sector')
+st.plotly_chart(fig)
 
-with col2:
-    st.header('Second sankey')
 
-    test = df.groupby(['year', 'sector name', 'item'])['cop'].sum().reset_index()
-    test['PGN'] = 'PGN'
-    test = test[test['year'] == year]
 
-    test['sector name'] = test['sector name'].map(dic_info)
-    test['item'] = test['item'].map(dic_info)
-
-    fig = go.Figure(data=[go.Sankey(
-        node = dict(
-        pad = 15,
-        thickness = 20,
-        line = dict(color = "black", width = 0.5),
-        label = list(dic_info.keys()),
-        color = "blue"
-        ),
-        link = dict(
-        source = test['sector name'], # indices correspond to labels, eg A1, A2, A1, B1, ...
-        target = test['item'],
-        value = test['cop']
-    ))])
-
-    fig.update_layout(title_text="Basic Sankey Diagram", font_size=10, width=500, height=450)
-    st.plotly_chart(fig)
 
 
 
